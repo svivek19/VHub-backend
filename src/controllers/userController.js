9b44c947629cc86b619331ea7a92b219e58297c7
svivek19 <svivek030503@gmail.com>
2026-03-03 18:27:57 +0530

    feat: add delete user functionality to user controller and routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,3 +57,19 @@ export const updateUser = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getAllUsers, updateUser } from "../controllers/userController.js";
+import {
+  deleteUser,
+  getAllUsers,
+  updateUser,
+} from "../controllers/userController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", protect, getAllUsers);
 userRouter.put("/:id", protect, updateUser);
+userRouter.delete("/:id", protect, deleteUser);
 
 export default userRouter;
